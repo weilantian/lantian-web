bb231cc1cafdf70316a31880fb07543f2d153beb
lantianwei <xiaolanlub@outlook.com>
2023-02-04 17:22:00 +1100

    audit

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -20,7 +20,9 @@ const Navbar: FC<{
           src="/logo.png"
         />
 
-        {withName && <span className="hidden md:block"> Lantian Wei</span>}
+        {withName && (
+          <span className="hidden md:block font-semibold"> Lantian Wei</span>
+        )}
       </span>
       <div className="flex md:flex-none flex-1 md:w-auto w-full relative w-8/12 items-center">
         <button className="absolute  md:hidden flex items-center justify-center z-10 right-0 text-white w-12 h-12 rounded-full bg-blue-500">
@@ -35,8 +37,8 @@ const Navbar: FC<{
         ></span>
         <div className="overflow-scroll scrollbar-hide md:overflow-auto">
           <div className=" py-4 md:static   ">
-            <ul className="flex ml-4   md:w-auto w-[320px]  md:space-x-7 space-x-5 md:mr-12  mr-8">
-              <li className="inline-block mr-4">
+            <ul className="flex ml-4 font-medium  md:w-auto w-[320px]  md:space-x-7 space-x-5 md:mr-12  mr-8">
+              <li className="inline-block ">
                 <a
                   className="hover:text-gray-400 relative transition-colors duration-75"
                   href="#home"
@@ -44,7 +46,7 @@ const Navbar: FC<{
                   Home
                 </a>
               </li>
-              <li className="inline-block mr-4">
+              <li className="inline-block">
                 <a
                   className="hover:text-gray-400 transition-colors duration-75"
                   href="#about"
@@ -52,7 +54,7 @@ const Navbar: FC<{
                   About
                 </a>
               </li>
-              <li className="inline-block mr-4">
+              <li className="inline-block">
                 <a
                   className="hover:text-gray-400 transition-colors duration-75"
                   href="#projects"
