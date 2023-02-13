402b0362a9ad36db3f8c4ed4d53e547f42510c29
lantianwei <xiaolanlub@outlook.com>
2023-02-13 18:24:32 +1100

    refactor: opt font load

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -6,18 +6,6 @@ import Head from "next/head";
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <Layout>
-      <Head>
-        <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link
-          rel="preconnect"
-          href="https://fonts.gstatic.com"
-          crossOrigin=""
-        />
-        <link
-          href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,300;0,400;0,500;0,600;1,400;1,500;1,600&display=swap"
-          rel="stylesheet"
-        />
-      </Head>
       <Component {...pageProps} />
     </Layout>
   );
