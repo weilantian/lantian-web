05f0a3383ffa6ef1689442805bc384cfc30d56b5
lantianwei <xiaolanlub@outlook.com>
2023-02-21 16:36:19 +1100

    feat: linkedin redirect

--- next.config.js
+++ next.config.js
@@ -1,6 +1,22 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-}
+  async redirects() {
+    return [
+      {
+        source: "/:path*",
+        has: [
+          {
+            type: "header",
+            key: "Host",
+            value: "linkedin.ericwei.fyi",
+          },
+        ],
+        permanent: true,
+        destination: "https://www.linkedin.com/in/eric-wei-92a0b2171/",
+      },
+    ];
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
