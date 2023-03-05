15dd09236583968ca319cc0b8b4e1e510c53a692
lantianwei <xiaolanlub@outlook.com>
2023-03-05 20:57:40 +1100

    feat: better turbine

--- components/CanvasSkillCard.tsx
+++ components/CanvasSkillCard.tsx
@@ -4,27 +4,34 @@ import { Environment, OrbitControls, Stage, Sky } from "@react-three/drei";
 import { Canvas } from "@react-three/fiber";
 import type { OrbitControls as OrbitControlsImpl } from "three-stdlib";
 import TurbineModel from "./TurbineModel";
+import { Group } from "three";
 
 const CanvasSkillCard: FC = () => {
+  const modelRef = useRef<Group>(null!);
   return (
     <div className="w-[200px] webkit-overflow-hidden relative overflow-hidden  rounded-xl h-[200px]">
       <Canvas
         resize={{ scroll: false }}
         className="w-full h-full rounded-xl"
-        camera={{ fov: 50 }}
+        camera={{
+          fov: 50,
+          near: 0.1,
+          rotation: [0, 1, 0],
+          far: 1000,
+          position: [90, 20, 40],
+        }}
         shadows
         dpr={[1, 2]}
       >
+        <Environment preset="dawn" />
         <Sky
           distance={450000}
           sunPosition={[5, 1, 8]}
           inclination={0}
           azimuth={0.25}
         />
-        <Stage environment="city" intensity={1} preset="rembrandt">
-          <TurbineModel />
-        </Stage>
-        <OrbitControls autoRotate />
+
+        <TurbineModel ref={modelRef} />
       </Canvas>
       <div
         style={{
