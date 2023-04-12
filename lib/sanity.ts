cc5e6ab296e936ca73bf5bf634bded5019115ac0
lantianwei <xiaolanlub@outlook.com>
2023-04-12 11:11:38 +1000

    feat: build error fixing

--- lib/sanity.ts
+++ lib/sanity.ts
@@ -4,7 +4,7 @@ import imageUrlBuilder from "@sanity/image-url";
 
 export const client = createClient({
   projectId: "s55i3zss",
-  dataset: "production",
+  dataset: process.env.NODE_ENV == "production" ? "production" : "dev",
   useCdn: true,
   apiVersion: "v2021-10-21",
 });
