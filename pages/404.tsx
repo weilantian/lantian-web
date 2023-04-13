37d9ae7326ea25313191e09081c82b364c661692
lantianwei <xiaolanlub@outlook.com>
2023-04-13 21:43:13 +1000

    feat: warn icon

--- pages/404.tsx
+++ pages/404.tsx
@@ -8,7 +8,7 @@ const FourOFourPage = () => {
         <title>Page not found - Eric Wei</title>
       </Head>
       <div className=" bg-white px-8 pb-12 pt-8 rounded-lg  ">
-        <h1 className="text-2xl font-bold">This page can not be found...</h1>
+        <h1 className="text-2xl font-bold">⚠️ This page can not be found...</h1>
         <Link className="mt-6 font-medium text-blue-500 block" href="/">
           Back to Homepage
         </Link>
