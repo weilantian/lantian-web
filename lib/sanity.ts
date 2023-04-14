357559c10e8e874c264bc74baec8b0e011187531
lantianwei <xiaolanlub@outlook.com>
2023-04-14 10:44:24 +1000

    feat: project preview

--- lib/sanity.ts
+++ lib/sanity.ts
@@ -1,16 +1,26 @@
 import { createClient } from "next-sanity";
 
+import { definePreview } from "next-sanity/preview";
+
 import imageUrlBuilder from "@sanity/image-url";
+
+const dataset = process.env.NEXT_PUBLIC_FORCE_PRODUCTION ? "production" : "dev";
 
 export const client = createClient({
   projectId: "s55i3zss",
-  dataset:
-    process.env.NODE_ENV == "production" ||
-    process.env.NEXT_PUBLIC_FORCE_PRODUCTION
-      ? "production"
-      : "dev",
+  dataset,
   useCdn: true,
   apiVersion: "v2021-10-21",
+});
+
+function onPublicAccessOnly() {
+  throw new Error(`Unable to load preview as you're not logged in`);
+}
+
+export const usePreview = definePreview({
+  projectId: "s55i3zss",
+  dataset,
+  onPublicAccessOnly,
 });
 
 export const urlFor = (source: any) => imageUrlBuilder(client).image(source);
