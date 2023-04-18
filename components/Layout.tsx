4dc2362892897b8df56250a2208b01d4f128866a
lantianwei <xiaolanlub@outlook.com>
2023-04-18 14:12:33 +1000

    feat: add ga

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -2,6 +2,7 @@ import Head from "next/head";
 import { FC, PropsWithChildren } from "react";
 import Footer from "./Footer";
 import Navbar from "./Navbar";
+import Script from "next/script";
 
 const Layout: FC<PropsWithChildren<{}>> = ({ children }) => {
   return (
@@ -12,6 +13,18 @@ const Layout: FC<PropsWithChildren<{}>> = ({ children }) => {
       <Navbar />
       {children}
       <Footer />
+      <Script
+        src="https://www.googletagmanager.com/gtag/js?id=G-E429VC1L6G"
+        strategy="afterInteractive"
+      ></Script>
+      <Script id="google-analytics" strategy="afterInteractive">
+        {`
+        window.dataLayer = window.dataLayer || [];
+        function gtag(){dataLayer.push(arguments);}
+        gtag('js', new Date());
+        gtag('config', 'G-E429VC1L6G');
+       `}
+      </Script>
     </div>
   );
 };
